d986691aecc33ad7037aff6e0ee67468f0f10e3a
Veronica Melican <vrmelican@wpi.edu>
2019-11-18 11:20:46 -0500

    Added CreatePlaylistRequest and CreatePlaylistResponse http files

--- VideoApp/src/main/java/http/CreatePlaylistRequest.java
+++ VideoApp/src/main/java/http/CreatePlaylistRequest.java
@@ -2,4 +2,20 @@ package http;
 
 public class CreatePlaylistRequest {
 
+	public String name;
+	
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+	
+	public CreatePlaylistRequest() {
+	}
+	
+	public CreatePlaylistRequest(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return "CreatePlaylist(" + name + ")";
+	}
+	
 }

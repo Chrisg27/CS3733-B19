8972f629413a9bc4050770375bc7b8a7828a614c
Chrisg27 <chrisg2706@gmail.com>
2019-11-24 21:01:38 -0500

    Added folders for html and js files and made js files for use cases

---
+++ VideoApp/js/api.js
@@ -0,0 +1,6 @@
+// all access driven through BASE. Must end with a SLASH
+// be sure you change to accommodate your specific API Gateway entry point
+var baseURL = "https://qu71sq0yv8.execute-api.us-east-1.amazonaws.com/beta/"; 
+
+var getPlaylistsURL = baseURL + "playlists";   // GET
+var getVideosURL = baseURL + "videos";     // GET

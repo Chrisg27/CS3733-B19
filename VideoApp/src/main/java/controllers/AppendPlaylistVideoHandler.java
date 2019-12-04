8431445d42b37ca785e5914b3b1388a080f9e30f
Veronica Melican <vrmelican@wpi.edu>
2019-12-03 19:37:47 -0500

    Created lambda functions to append and remove videos from playlists

---
+++ VideoApp/src/main/java/controllers/AppendPlaylistVideoHandler.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import dataBase.PlaylistsDAO;
+import dataBase.VideosDAO;
+import http.PlaylistVideoRequest;
+import http.PlaylistVideoResponse;
+import model.Playlist;
+import model.VideoClip;
+
+public class AppendPlaylistVideoHandler implements RequestHandler<PlaylistVideoRequest, PlaylistVideoResponse> {
+
+	LambdaLogger logger;
+	
+	boolean appendVideo(String playlistName, String videoUrl) throws Exception {
+		if (logger != null) { logger.log("in appendVideo"); }
+		PlaylistsDAO daoPlaylist = new PlaylistsDAO();
+		VideosDAO daoVideo = new VideosDAO();
+		
+		Playlist existPlaylist = daoPlaylist.getPlaylist(playlistName);
+		VideoClip existVideo = daoVideo.getVideoClip(videoUrl);
+		
+		if (existPlaylist != null && existVideo != null) {
+			return daoPlaylist.addVideoClipToPlaylist(existPlaylist, existVideo);
+		}
+		else {
+			return false;
+		}
+	}
+	
+	@Override
+    public PlaylistVideoResponse handleRequest(PlaylistVideoRequest req, Context context) {
+    	logger = context.getLogger();
+    	logger.log("Loading Java handler to append video to playlist");
+    	
+    	PlaylistVideoResponse response;
+    	logger.log(req.toString());
+    	
+    	try {
+    		if(appendVideo(req.playlist, req.videoURL)) {
+    			response = new PlaylistVideoResponse(req.playlist, 200);
+    		}
+    		else {
+    			response = new PlaylistVideoResponse(req.playlist, 422);
+    		}
+    	} catch (Exception e) {
+    		response = new PlaylistVideoResponse("Unable to append video to playlist: " + req.playlist + "(" + e.getMessage() + ")", 400);
+    	}
+    	
+    	return response;
+    }
+
+}
---
+++ VideoApp/src/main/java/controllers/RemovePlaylistVideoHandler.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import dataBase.PlaylistsDAO;
+import dataBase.VideosDAO;
+import http.PlaylistVideoRequest;
+import http.PlaylistVideoResponse;
+import model.Playlist;
+import model.VideoClip;
+
+public class RemovePlaylistVideoHandler implements RequestHandler<PlaylistVideoRequest, PlaylistVideoResponse> {
+
+	LambdaLogger logger;
+	
+	boolean removeVideo(String playlistName, String videoUrl) throws Exception {
+		if (logger != null) { logger.log("in appendVideo"); }
+		PlaylistsDAO daoPlaylist = new PlaylistsDAO();
+		VideosDAO daoVideo = new VideosDAO();
+		
+		Playlist existPlaylist = daoPlaylist.getPlaylist(playlistName);
+		VideoClip existVideo = daoVideo.getVideoClip(videoUrl);
+		
+		if (existPlaylist != null && existVideo != null) {
+			return daoPlaylist.deleteVideoClipFromPlaylist(existPlaylist, existVideo);
+		}
+		else {
+			return false;
+		}
+	}
+	
+	@Override
+    public PlaylistVideoResponse handleRequest(PlaylistVideoRequest req, Context context) {
+    	logger = context.getLogger();
+    	logger.log("Loading Java handler to remove video from playlist");
+    	
+    	PlaylistVideoResponse response;
+    	logger.log(req.toString());
+    	
+    	try {
+    		if(removeVideo(req.playlist, req.videoURL)) {
+    			response = new PlaylistVideoResponse(req.playlist, 200);
+    		}
+    		else {
+    			response = new PlaylistVideoResponse(req.playlist, 422);
+    		}
+    	} catch (Exception e) {
+    		response = new PlaylistVideoResponse("Unable to remove video from playlist: " + req.playlist + "(" + e.getMessage() + ")", 400);
+    	}
+    	
+    	return response;
+    }
+
+}

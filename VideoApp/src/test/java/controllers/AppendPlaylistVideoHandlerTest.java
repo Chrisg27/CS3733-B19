fac6aa36a70dc769e3a8f64f8911350c804e8bbb
Veronica Melican <vrmelican@wpi.edu>
2019-12-04 07:35:29 -0500

    Lambda function to search local videos

--- VideoApp/src/test/java/controllers/AppendPlaylistVideoHandlerTest.java
+++ VideoApp/src/test/java/controllers/AppendPlaylistVideoHandlerTest.java
@@ -11,33 +11,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 /**
  * A simple test harness for locally invoking your Lambda function handler.
  */
-public class AppendPlaylistVideoHandlerTest {
+public class AppendPlaylistVideoHandlerTest extends LambdaTest {
 
-    private static Object input;
-
-    @BeforeClass
-    public static void createInput() throws IOException {
-        // TODO: set up your sample input object here.
-        input = null;
-    }
-
-    private Context createContext() {
-        TestContext ctx = new TestContext();
-
-        // TODO: customize your context here if needed.
-        ctx.setFunctionName("Your Function Name");
-
-        return ctx;
-    }
-
-    @Test
-    public void testAppendPlaylistVideoHandler() {
-        AppendPlaylistVideoHandler handler = new AppendPlaylistVideoHandler();
-        Context ctx = createContext();
-
-        //String output = handler.handleRequest(input, ctx);
-
-        // TODO: validate output here if needed.
-        //Assert.assertEquals("Hello from Lambda!", output);
-    }
+    
+	
 }

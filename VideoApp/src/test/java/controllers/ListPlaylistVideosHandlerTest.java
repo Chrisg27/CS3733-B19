37f89eadc150717d9d46fa7d277717054230f4f2
Veronica Melican <vrmelican@wpi.edu>
2019-12-04 19:17:45 -0500

    JUnit tests for listing videos in a playlist

--- VideoApp/src/test/java/controllers/ListPlaylistVideosHandlerTest.java
+++ VideoApp/src/test/java/controllers/ListPlaylistVideosHandlerTest.java
@@ -8,10 +8,29 @@ import org.junit.Test;
 
 import com.amazonaws.services.lambda.runtime.Context;
 
+import http.PlaylistRequest;
+import http.PlaylistResponse;
+import http.ListPlaylistVideosResponse;
+
 /**
  * A simple test harness for locally invoking your Lambda function handler.
  */
-public class ListPlaylistVideosHandlerTest {
+public class ListPlaylistVideosHandlerTest extends LambdaTest {
 
-   
+   @Test
+   public void testListPlaylistVideosHandler() {
+	   //create test playlist
+	   PlaylistRequest pr = new PlaylistRequest("LambdaTestPlaylist");
+	   PlaylistResponse pResponse = new CreatePlaylistHandler().handleRequest(pr, createContext("create"));
+	   
+	   //test list videos
+	   ListPlaylistVideosResponse response = new ListPlaylistVideosHandler() .handleRequest(pr, createContext("list"));
+	   Assert.assertEquals(200, response.statusCode);
+	   
+	   //test with playlist that does not exist
+	   PlaylistRequest pr2 = new PlaylistRequest("PlaylistNoExist");
+	   response = new ListPlaylistVideosHandler().handleRequest(pr2, createContext("list"));
+	   Assert.assertEquals(403, response.statusCode);
+   }
+	
 }

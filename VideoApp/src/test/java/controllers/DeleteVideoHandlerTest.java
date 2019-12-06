afa46a8b15f3a68e50f24d6b133e51db9ae46a51
Veronica Melican <vrmelican@wpi.edu>
2019-12-06 13:27:03 -0500

    edit test cases

--- VideoApp/src/test/java/controllers/DeleteVideoHandlerTest.java
+++ VideoApp/src/test/java/controllers/DeleteVideoHandlerTest.java
@@ -18,32 +18,10 @@ import http.UploadVideoResponse;
  */
 public class DeleteVideoHandlerTest extends LambdaTest{
 
-    private static Object input;
-
-    @BeforeClass
-    public static void createInput() throws IOException {
-        // TODO: set up your sample input object here.
-        input = null;
-    }
-
-    private Context createContext() {
-        TestContext ctx = new TestContext();
-
-        // TODO: customize your context here if needed.
-        ctx.setFunctionName("DeleteVideoHandler");
-
-        return ctx;
-    }
-
-    @Test
+      @Test
     public void testDeleteVideoHandler() {
     	DeleteVideoRequest req = new DeleteVideoRequest("LambdaTestUploadVideo", "", "", false);
     	DeleteVideoResponse res = new DeleteVideoHandler().handleRequest(req, createContext("DeleteVideoHandler"));
     	Assert.assertEquals("LambdaTestUploadVideo", res.response);
-
-        //String output = handler.handleRequest(input, ctx);
-
-        // TODO: validate output here if needed.
-        //Assert.assertEquals("Hello from Lambda!", output);
     }
 }
--- VideoApp/src/test/java/controllers/UploadVideoHandlerTest.java
+++ VideoApp/src/test/java/controllers/UploadVideoHandlerTest.java
@@ -17,42 +17,10 @@ import http.UploadVideoResponse;
  */
 public class UploadVideoHandlerTest extends LambdaTest{
 
-    private static Object input;
-
-    @BeforeClass
-    public static void createInput() throws IOException {
-        // TODO: set up your sample input object here.
-        input = null;
-    }
-
-    private Context createContext() {
-        TestContext ctx = new TestContext();
-
-        // TODO: customize your context here if needed.
-        ctx.setFunctionName("UploadVideoHandler");
-
-        return ctx;
-    }
-
     @Test
     public void testUploadVideo() {
     	UploadVideoRequest req = new UploadVideoRequest("LambdaTestUploadVideo", "", "", "");
-    	UploadVideoResponse res = new UploadVideoHandler().handleRequest(req, createContext("UploadVideoHandler"));
+    	UploadVideoResponse res = new UploadVideoHandler().handleRequest(req, createContext("upload"));
     	Assert.assertEquals("LambdaTestUploadVideo", res.response);
-    	
-    	//res = new UploadVideoHandler().handleRequest(req, createContext("create"));
-		//Assert.assertEquals(422, res.httpCode);
-		
-		//UploadVideoResponse deleteResponse = new DeleteVideoHandler().handleRequest(req, createContext("delete"));
-		//Assert.assertEquals("LambdaTestUploadVideo", deleteResponse.response);
-    	
-    	
-        //UploadVideoHandler handler = new UploadVideoHandler();
-        //Context ctx = createContext();
-
-        //String output = handler.handleRequest(input, ctx);
-
-        // TODO: validate output here if needed.
-        //Assert.assertEquals("Hello from Lambda!", output);
     }
 }
